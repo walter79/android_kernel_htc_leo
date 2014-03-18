34c689d2102ac82e1f19cb8a507a371eb6c38f78
walter79 <gzwalter79@gmail.com>
2014-03-18 21:07:44 +0100

    update xattr support
    Using yaffs2 from Samsung Nexus S (crespo) 2.6.39 kernel

--- fs/yaffs2/yaffs_vfs.c
+++ fs/yaffs2/yaffs_vfs.c
@@ -42,6 +42,7 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/security.h>
 #include <linux/proc_fs.h>
 #include <linux/pagemap.h>
 #include <linux/mtd/mtd.h>
@@ -65,7 +66,7 @@
 
 #define YPROC_ROOT  NULL
 
-#define Y_INIT_TIMER(a, b, c)	setup_deferrable_timer_on_stack(a, b, c)
+#define Y_INIT_TIMER(a)	init_timer_on_stack(a)
 
 #define WRITE_SIZE_STR "writesize"
 #define WRITE_SIZE(mtd) ((mtd)->writesize)
@@ -201,6 +202,50 @@ struct inode *yaffs_get_inode(struct super_block *sb, int mode, int dev,
 	return inode;
 }
 
+#ifdef CONFIG_YAFFS_XATTR
+static int yaffs_init_security(struct inode *dir, struct dentry *dentry,
+			       struct inode *inode)
+{
+	int err;
+	size_t size;
+	void *value;
+	char *suffix;
+	char name[XATTR_NAME_MAX];
+	struct yaffs_dev *dev;
+	struct yaffs_obj *obj = yaffs_inode_to_obj(inode);
+	int result;
+
+	err = security_inode_init_security(inode, dir, &dentry->d_name,
+					   &suffix, &value, &size);
+	if (err) {
+		if (err == -EOPNOTSUPP)
+			return 0;
+		return err;
+	}
+	snprintf(name, sizeof name, "%s%s", XATTR_SECURITY_PREFIX, suffix);
+
+	/* inlined yaffs_setxattr: no instantiated dentry yet */
+	dev = obj->my_dev;
+	yaffs_gross_lock(dev);
+	result = yaffs_set_xattrib(obj, name, value, size, 0);
+	if (result == YAFFS_OK)
+		err = 0;
+	else if (result < 0)
+		err = result;
+	yaffs_gross_unlock(dev);
+
+	kfree(value);
+	kfree(suffix);
+	return err;
+}
+#else
+static int yaffs_init_security(struct inode *dir, struct dentry *dentry,
+			       struct inode *inode)
+{
+	return 0;
+}
+#endif
+
 static int yaffs_mknod(struct inode *dir, struct dentry *dentry, int mode,
 		       dev_t rdev)
 {
@@ -266,6 +311,7 @@ static int yaffs_mknod(struct inode *dir, struct dentry *dentry, int mode,
 
 	if (obj) {
 		inode = yaffs_get_inode(dir->i_sb, mode, rdev, obj);
+		yaffs_init_security(dir, dentry, inode);
 		d_instantiate(dentry, inode);
 		update_dir_time(dir);
 		yaffs_trace(YAFFS_TRACE_OS,
@@ -353,6 +399,7 @@ static int yaffs_symlink(struct inode *dir, struct dentry *dentry,
 		struct inode *inode;
 
 		inode = yaffs_get_inode(dir->i_sb, obj->yst_mode, 0, obj);
+		yaffs_init_security(dir, dentry, inode);
 		d_instantiate(dentry, inode);
 		update_dir_time(dir);
 		yaffs_trace(YAFFS_TRACE_OS, "symlink created OK");
@@ -1674,9 +1721,10 @@ static int yaffs_bg_thread_fn(void *data)
 		if (time_before(expires, now))
 			expires = now + HZ;
 
-		Y_INIT_TIMER(&timer, yaffs_background_waker,
-				(unsigned long)current);
+		Y_INIT_TIMER(&timer);
 		timer.expires = expires + 1;
+		timer.data = (unsigned long)current;
+		timer.function = yaffs_background_waker;
 
 		set_current_state(TASK_INTERRUPTIBLE);
 		add_timer(&timer);
@@ -2373,17 +2421,19 @@ static int yaffs_internal_read_super_mtd(struct super_block *sb, void *data,
 	return yaffs_internal_read_super(1, sb, data, silent) ? 0 : -EINVAL;
 }
 
-static struct dentry *yaffs_mount(struct file_system_type *fs, int flags,
-			const char *dev_name, void *data)
+static struct dentry *yaffs_read_super(struct file_system_type *fs,
+				       int flags, const char *dev_name,
+				       void *data)
 {
+
 	return mount_bdev(fs, flags, dev_name, data,
-		yaffs_internal_read_super_mtd);
+			  yaffs_internal_read_super_mtd);
 }
 
 static struct file_system_type yaffs_fs_type = {
 	.owner = THIS_MODULE,
 	.name = "yaffs",
-	.mount = yaffs_mount,
+	.mount = yaffs_read_super,
 	.kill_sb = kill_block_super,
 	.fs_flags = FS_REQUIRES_DEV,
 };
@@ -2396,17 +2446,17 @@ static int yaffs2_internal_read_super_mtd(struct super_block *sb, void *data,
 	return yaffs_internal_read_super(2, sb, data, silent) ? 0 : -EINVAL;
 }
 
-static struct dentry *yaffs2_mount(struct file_system_type *fs,
-			     int flags, const char *dev_name, void *data)
+static struct dentry *yaffs2_read_super(struct file_system_type *fs, int flags,
+					const char *dev_name, void *data)
 {
 	return mount_bdev(fs, flags, dev_name, data,
-			   yaffs2_internal_read_super_mtd);
+			  yaffs2_internal_read_super_mtd);
 }
 
 static struct file_system_type yaffs2_fs_type = {
 	.owner = THIS_MODULE,
 	.name = "yaffs2",
-	.mount = yaffs2_mount,
+	.mount = yaffs2_read_super,
 	.kill_sb = kill_block_super,
 	.fs_flags = FS_REQUIRES_DEV,
 };
